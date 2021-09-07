6b17bb6eb59bd097ef8cb0a69e09e4de91bcda74
SHLee97 <87516295+SHLee97@users.noreply.github.com>
2021-09-07 18:19:05 +0900

    210907

---
+++ src/main2.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int main(int argc, char **argv){
+
+    signal(SIGINT, signal_handler); // to exit program when ctrl+c
+
+    ros::init(argc, argv, "drone_simulator");
+    ros::NodeHandle n("~");
+
+    Simulator sim(n);
+
+    ros::AsyncSpinner spinner(7); // Use 5 threads -> 5 callbacks
+    spinner.start();
+    ros::waitForShutdown();
+
+    return 0;
+}
